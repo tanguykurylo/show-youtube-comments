b388972d395825bdcba5877f6d2e90bfcd12fed2
Tanguy Kurylo <t.kurylo@gmail.com>
2021-05-07 14:34:35 +0200

    Fix scrolling the entire page when comments load
    YouTube loads new comments when an over-scroll event is triggered on  #secondary-inner. Unfortunately this over-scroll event requires scrolling the entire page if #secondary manages scrolling.
    This fix moves the scrolling behavior to the #secondary container. Over-scrolling on #secondary is also disabled to let the comments load in #secondary-inner without scrolling the entire page.

--- showYoutubeComments.js
+++ showYoutubeComments.js
@@ -24,16 +24,15 @@ function panelsContainNodes() {
 }
 
 function makeCommentsScrollable() {
-	let margins = rightPanelMargins();
-	rightPanel().style.height = window.innerHeight - margins.top + "px";
-	rightPanel().style.overflow = "scroll";
+	let margins = computeMargins(rightPanelContainer())
+	rightPanelContainer().style.height = window.innerHeight - margins.top + "px";
 }
 
-function rightPanelMargins() {
-	let rightPanelStyle = window.getComputedStyle(rightPanel());
+function computeMargins(element) {
+	let style = window.getComputedStyle(element);
 	let navbarStyle = window.getComputedStyle(navbar());
-	let marginLeft = parseInt(rightPanelStyle.marginLeft);
-	let marginTop = parseInt(rightPanelStyle.paddingTop) + parseInt(navbarStyle.height);
+	let marginLeft = parseInt(style.marginLeft);
+	let marginTop = parseInt(style.paddingTop) + parseInt(navbarStyle.height);
 	return {
 		left: marginLeft,
 		top: marginTop
@@ -44,6 +43,9 @@ function rightPanel() {
 	return document.getElementById('secondary-inner');
 }
 
+function rightPanelContainer() {
+	return document.getElementById('secondary');
+}
 
 function leftPanel() {
 	return document.getElementById('primary-inner');
